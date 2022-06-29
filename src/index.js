3408cea28e8ed63f6324aff59f30008eaa74ef8d
Irina Akhanteva <irinkayashina@gmail.com>
2022-06-28 23:17:08 -1100

    feat: modify function toReadable

--- src/index.js
+++ src/index.js
@@ -1,3 +1,71 @@
 module.exports = function toReadable (number) {
-  
+  const ones = [
+    '',
+    'one',
+    'two',
+    'three',
+    'four',
+    'five',
+    'six',
+    'seven',
+    'eight',
+    'nine',
+    'ten',
+    'eleven',
+    'twelve',
+    'thirteen',
+    'fourteen',
+    'fifteen',
+    'sixteen',
+    'seventeen',
+    'eighteen',
+    'nineteen'
+  ];
+  const tens = [
+    '',
+    '',
+    'twenty',
+    'thirty',
+    'forty',
+    'fifty',
+    'sixty',
+    'seventy',
+    'eighty',
+    'ninety'
+  ];
+
+  let numberString = number.toString();
+  let result = '';
+
+  if(number === 0) {
+    return 'zero';
+  }
+
+  else {
+    let hundredCount = Math.floor(number / 100);
+    let tenCount = Math.floor(number % 100 / 10);
+    let oneCount = number % 10;
+
+    if(hundredCount >= 1) {
+      result += ones[hundredCount] + ' hundred';
+      if (number % 100 !== 0) {
+         result += ' ';
+      }
+    }
+
+    if(tenCount > 1) {
+      if (oneCount !== 0) {
+        result += tens[tenCount] + ' ' + ones[oneCount];
+      }
+      else {
+        result += tens[tenCount] + ones[oneCount];
+      }
+    }
+
+    else {
+      result += ones[number % 100];
+    }
+
+    return result;
+  }
 }
